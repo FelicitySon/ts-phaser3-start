f0b8a94afbf8962130265a78186478d4d541fdc3
EditYJ <158392613@qq.com>
2020-06-11 15:36:21 +0800

    添加场景背景

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -10,6 +10,11 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(0, 0, 'background').setOrigin(0, 0)
+    // 得到游戏窗口大小
+    const width = this.scale.width
+    const height = this.scale.height
+
+    // 添加以瓦片精灵的方式将背景图加入场景
+    this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0)
   }
 }
