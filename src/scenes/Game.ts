56842372f3edd0b3eea887932f8d50a4ef30c79d
EditYJ <158392613@qq.com>
2020-06-11 16:47:36 +0800

    火箭鼠初步已加入战斗

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,6 +7,9 @@ export default class Game extends Phaser.Scene {
 
   preload() {
     this.load.image('background', 'house/bg_repeat_340x640.png')
+
+    // 加载角色
+    this.load.atlas('rocket-mouse', 'characters/rocket-mouse.png', 'characters/rocket-mouse.json')
   }
 
   create() {
@@ -14,7 +17,23 @@ export default class Game extends Phaser.Scene {
     const width = this.scale.width
     const height = this.scale.height
 
+    this.anims.create({
+      key: 'rocket-mouse-run',
+      frames: this.anims.generateFrameNames('rocket-mouse', {
+        start: 1,
+        end: 4,
+        prefix: 'rocketmouse_run',
+        zeroPad: 2,
+        suffix: '.png'
+      }),
+      frameRate: 10,
+      repeat: -1
+    })
+
     // 添加以瓦片精灵的方式将背景图加入场景
     this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0)
+
+    // 添加角色
+    this.add.sprite(width * 0.5, height * 0.5, 'rocket-mouse', 'rocketmouse_fly01.png').play('rocket-mouse-run')
   }
 }
